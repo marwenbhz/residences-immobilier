04be5eeff6ce9cd7599fd32fcb9cde942843cb1a
Marwen Ben Hriz <marwen.benhriz@gmail.com>
2019-01-02 09:46:02 +0100

    initial commit

---
+++ residences_immobilier/spiders/residences_spider.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy import Request
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.crawler import CrawlerProcess
+
+
+class ResidencesSpiderSpider(scrapy.Spider):
+    name = 'residences_spider'
+    allowed_domains = ['residences-immobilier.com']
+    start_urls = ['https://www.residences-immobilier.com/fr/annonces.html']
+    custom_settings = {
+    'LOG_FILE': 'logs/residences-immobilier.log',
+    'LOG_LEVEL':'ERROR'
+     }
+
+
+
+    def __init__(self, *args, **kwargs):
+        super(ResidencesSpiderSpider, self).__init__(*args, **kwargs)
+
+
+    def parse(self, response):
+        print('PROCESSIGN...' + response.url)
